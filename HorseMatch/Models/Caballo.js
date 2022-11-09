633e5e0574f9dec97f1cd5060bb31e89e78d257b
joseantonio2001 <joseantoniotorrescoca@gmail.com>
2022-11-09 12:11:59 +0100

    Asignando dato miembro descansado a clase caballo. Closes #14

--- HorseMatch/Models/Caballo.js
+++ HorseMatch/Models/Caballo.js
@@ -5,10 +5,12 @@ class Caballo{
      * Constructor clase Caballo
      * @param {String} nombre Identificador del caballo 
      * @param {Nivel} nivel Nivel del caballo
+     * @param {boolean} descansado Caballo está descansado o no
      */
-    constructor(nombre, nivel){
+    constructor(nombre, nivel, descansado){
         this.nombre = nombre;
         this.nivel = nivel;
+        this.descansado = descansado;
     }
 }
 
