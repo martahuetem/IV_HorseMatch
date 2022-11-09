f5aac8990db135b252296ceaf30de558a5d19169
joseantonio2001 <joseantoniotorrescoca@gmail.com>
2022-11-09 11:59:14 +0100

    Asignando dato miembro nivel a clase caballo. Avanza en el issue #14

---
+++ HorseMatch/Models/Caballo.js
@@ -0,0 +1,15 @@
+const Nivel = require("../Enums/Nivel")
+
+class Caballo{
+    /**
+     * Constructor clase Caballo
+     * @param {String} nombre Identificador del caballo 
+     * @param {Nivel} nivel Nivel del caballo
+     */
+    constructor(nombre, nivel){
+        this.nombre = nombre;
+        this.nivel = nivel;
+    }
+}
+
+module.exports = Caballo;
