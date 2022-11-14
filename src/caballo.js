5b417db4edc5277868ee510f56a2ef7ab0af8d35
joseantonio2001 <joseantoniotorrescoca@gmail.com>
2022-11-14 10:23:54 +0100

    Elimina datos miembros no pertenecientes a esta fase del proyecto

--- src/caballo.js
+++ src/caballo.js
@@ -6,13 +6,11 @@ class caballo{
      * @param {String} nombre Identificador del caballo 
      * @param {nivel} nivel Nivel del caballo
      * @param {Boolean} descansado Caballo está descansado o no
-     * @param {Boolean} tieneJinete Caballo ya ha sido asignado a un Jinete o aún no.
      */
-    constructor(nombre, nivel, descansado, tieneJinete){
+    constructor(nombre, nivel, descansado){
         this.nombre = nombre;
         this.nivel = nivel;
         this.descansado = descansado;
-        this.tieneJinete = tieneJinete;
     }
     
 }
--- src/jinete.js
+++ src/jinete.js
@@ -6,13 +6,11 @@ class jinete{
      * @param {String} nombre Identificador del jinete 
      * @param {nivel} nivel Nivel del jinete
      * @param {Array} listaCaballos Caballos montados por el jinete
-     * @param {Boolean} tieneCaballo Jinete ya ha sido asignado a un Caballo o aún no.
      */
-    constructor(nombre, nivel, listaCaballos, tieneCaballo){
+    constructor(nombre, nivel, listaCaballos){
         this.nombre = nombre;
         this.nivel = nivel;
         this.listaCaballos = listaCaballos;
-        this.tieneCaballo = tieneCaballo;
     }
 
 }
