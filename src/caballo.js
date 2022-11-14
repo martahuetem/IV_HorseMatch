1e0662593c56ba173ccfdd591dc89e4146127359
joseantonio2001 <joseantoniotorrescoca@gmail.com>
2022-11-14 22:26:03 +0100

    Ajustando clase caballo a los cambios realizados

--- src/caballo.js
+++ src/caballo.js
@@ -1,8 +1,7 @@
 const Nivel = require("./nivel")
 /**
- * Declaración de la clase Caballo. Entidad (pues el valor de 
- * descansado podrá variar en función de si el caballo 
- * está disponible o no) que contendrá información del caballo.
+ * Declaración de la clase Caballo. Objeto-valor pues sus datos representarán
+ * un caballo y una vez inicializado no variarán sus datos miembros.
  */
 
 class Caballo{
@@ -10,12 +9,10 @@ class Caballo{
      * Constructor clase caballo
      * @param {String} nombre Identificador del caballo 
      * @param {Nivel} nivel Nivel del caballo
-     * @param {Boolean} descansado Disponibilidad del caballo
      */
-    constructor(nombre, nivel, descansado){
+    constructor(nombre, nivel){
         this.nombre = nombre;
         this.nivel = nivel;
-        this.descansado = descansado;
     }
     
 }
