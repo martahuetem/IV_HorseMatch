36c44de5d7613a44baf31973c5131994eee4d693
joseantonio2001 <joseantoniotorrescoca@gmail.com>
2022-11-14 18:03:21 +0100

    Añadiendo clases. Closes martahuetem#13

---
+++ src/caballo.js
@@ -0,0 +1,17 @@
+/**
+ * Declaración de la clase Caballo. Entidad que contendrá información 
+ * del caballo.
+ */
+
+class Caballo{
+    /**
+     * Constructor clase caballo
+     * @param {String} nombre Identificador del caballo 
+     */
+    constructor(nombre){
+        this.nombre = nombre;
+    }
+    
+}
+
+module.exports = Caballo;
---
+++ src/jinete.js
@@ -0,0 +1,18 @@
+
+/**
+ * Declaración de la clase Jinete. Objeto-valor que contendrá información 
+ * del jinete.
+ */
+
+class Jinete{
+    /**
+     * Constructor clase Jinete
+     * @param {String} nombre Identificador del jinete 
+     */
+    constructor(nombre){
+        this.nombre = nombre;
+    }
+
+}
+
+module.exports = Jinete;
