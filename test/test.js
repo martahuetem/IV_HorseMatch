70c600b6940831c49e5d147b7a920cfb32a93372
martahuetem <martahuete46@gmail.com>
2023-01-21 18:36:17 +0100

    Closes #22, #24. Creación del test para emparejar caballo y jinete.

---
+++ test/test.js
@@ -0,0 +1,45 @@
+const jinete = require("../src/jinete");
+const caballo = require("../src/caballo");
+
+
+function matchRiderWithHorse(jinetes, horses) {
+    jinetes.sort((a, b) => b.nivel - a.nivel);
+    horses.sort((a, b) => b.nivel - a.nivel);
+    return jinetes.map((jinete, nivel) => [jinete.nombre, horses[nivel].nombre]);
+}
+
+
+test('emparejar jinete con caballo', () => {
+    //Creo uno jinetes ejemplo
+    let marta = new jinete("Marta", 3);
+    let carlos = new jinete("Carlos", 2);
+    let paco = new jinete("Paco", 1);
+    let alberto = new jinete("Alberto", 0);
+
+    const jinetes = [marta, carlos, paco, alberto];
+
+    //Creo unos caballos ejemplo
+    let caballo1 = new caballo("A", 3);
+    let caballo2 = new caballo("B", 2);
+    let caballo3 = new caballo("C", 1);
+    let caballo4 = new caballo("D", 0);
+
+    const horses = [caballo1, caballo2, caballo3, caballo4];
+
+    //console.log(jinetes)
+    //console.log(horses)
+
+
+    const resultado_esperado = [["Marta", "A"], ["Carlos", "B"], ["Paco", "C"], ["Alberto", "D"]];
+
+    
+
+    const match = matchRiderWithHorse(jinetes, horses);
+    //console.log(match)
+    expect(match).toEqual(resultado_esperado);
+});
+
+
+
+
+
