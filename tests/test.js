ff173216fe428eadbeaf162f73877ce8a748d20e
martahuetem <martahuete46@gmail.com>
2023-01-26 10:47:56 +0100

    Avanzo #25. Simplifico el código del test

--- tests/test.js
+++ tests/test.js
@@ -29,40 +29,40 @@ function comprobar_matriz(matriz, nfil, ncol) {
 
 
  //Creo unos caballos ejemplo
- let caballo1 = new Caballo("A", 3);
- let caballo2 = new Caballo("B", 2);
- let caballo3 = new Caballo("C", 1);
- let caballo4 = new Caballo("D", 0);
-
- const horses = [caballo1, caballo2, caballo3, caballo4];
+ let horses = [
+    new Caballo("A", 3),
+    new Caballo("B", 2),
+    new Caballo("C", 1),
+    new Caballo("D", 0)
+ ]
 
  //Creo las preferencias de los alumnos a los caballos
  let pref_marta = [
-     new Map([[caballo1, 3]]),
-     new Map([[caballo2, 3]]),
-     new Map([[caballo3, 0]]),
-     new Map([[caballo4, 0]]),
+     new Map([[horses[0], 3]]),
+     new Map([[horses[1], 3]]),
+     new Map([[horses[2], 0]]),
+     new Map([[horses[3], 0]]),
  ];
 
  let pref_carlos = [
-     new Map([[caballo1, 0]]),
-     new Map([[caballo2, 3]]),
-     new Map([[caballo3, 3]]),
-     new Map([[caballo4, 0]]),
+     new Map([[horses[0], 0]]),
+     new Map([[horses[1], 3]]),
+     new Map([[horses[2], 3]]),
+     new Map([[horses[3], 0]]),
  ];
 
  let pref_paco = [
-     new Map([[caballo1, 0]]),
-     new Map([[caballo2, 0]]),
-     new Map([[caballo3, 3]]),
-     new Map([[caballo4, 0]]),
+     new Map([[horses[0], 0]]),
+     new Map([[horses[1], 0]]),
+     new Map([[horses[2], 3]]),
+     new Map([[horses[3], 0]]),
  ];
 
  let pref_alberto = [
-     new Map([[caballo1, 0]]),
-     new Map([[caballo2, 0]]),
-     new Map([[caballo3, 0]]),
-     new Map([[caballo4, 3]]),
+     new Map([[horses[0], 0]]),
+     new Map([[horses[1], 0]]),
+     new Map([[horses[2], 0]]),
+     new Map([[horses[3], 3]]),
  ];
 
  //Creo uno jinetes ejemplo
@@ -71,27 +71,30 @@ function comprobar_matriz(matriz, nfil, ncol) {
  let paco = new Jinete("Paco", 1, pref_paco);
  let alberto = new Jinete("Alberto", 0, pref_alberto);
 
+ let jinetes = [
+    new Jinete("Marta", 3, pref_marta),
+    new Jinete("Carlos", 2, pref_carlos),
+    new Jinete("Paco", 1, pref_paco),
+    new Jinete("Alberto", 0, pref_alberto)
+ ]
 
- const jinetes = [marta, carlos, alberto, paco];
-
- let njinetes = jinetes.length
- let ncaballos = horses.length
  let matriz = crear_matriz_costo(jinetes, horses)
 
  
 // TESTS //
 
 describe("Pruebas de corrección", () => {
-    
+
     test('El número de jinetes debe de ser menor o igual al de caballos', () => {
         //Comprobamos que el número de jinetes sea siempre menor o igual que el de caballos
+        let njinetes = jinetes.length
+        let ncaballos = horses.length
         expect(ncaballos).toBeGreaterThanOrEqual(njinetes);
     });
 
     test('Los costos de la matriz deben de ser mayores o iguales que 1', () => {
-        let matriz_valida = comprobar_matriz(matriz)
-
         //Compruebo que todos los costes de la matriz son mayores o iguales a 1
+        let matriz_valida = comprobar_matriz(matriz)
         expect(matriz_valida).toBeFalsy()
     });
 
@@ -107,7 +110,7 @@ describe("Pruebas de corrección", () => {
 
         const costo_esperado = 4
 
-        let caballos_asignados = [0, 1, 0, 0]
+        const caballos_asignados = [0, 1, 0, 0]
 
         let res = emparejamiento(matriz, horses.length, jinetes.length)
         let riders = new Array(jinetes.length)
